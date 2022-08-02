683fb9795bc6f349f3b3721c1c0481efb2099358
dianalleon <diana.kotoko115@gmail.com>
2022-08-02 11:52:34 -0500

    cambios

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 //Registrar Persona
-
 let personas = JSON.parse(localStorage.getItem('persona')) ?? [];
-let elimanadas = JSON.parse(localStorage.getItem('eliminada')) ?? [];
+let eliminadas = JSON.parse(localStorage.getItem('eliminada')) ?? [];
 
 mostrarTabla();
 mostrarPersonasEliminadas();
 
+//Ocultar boton de edición(boton inhabilitado)
+let editar = document.getElementById('editar')
+editar.style.display = "none"
+
+
 //Guardar Persona
 
 var formulario = document.getElementById('formulario');
+
 formulario.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const persona = {
         nombre: document.getElementById('nombre').value,
         cedula: document.getElementById('cedula').value,
-        fechaNacimiento: document.getElementById('fecha').value
+        fecha: document.getElementById('fecha').value
     }
 
     personas.push(persona);
-    persona.edad= calcularEdad(persona.fechaNacimiento);
+    persona.edad= calcularEdad(persona.fecha);
     mostrarTabla();
     localStorage.setItem('persona', JSON.stringify(personas));
 });
 
 
-function calcularEdad(fechaNacimiento){
+function calcularEdad(fecha){
     let hoy = new Date();
-    var cumpleanos = new Date(fechaNacimiento);
+    var cumpleanos = new Date(fecha);
     var edad = hoy.getFullYear() - cumpleanos.getFullYear();
     var m = hoy.getMonth() - cumpleanos.getMonth();
 
@@ -45,11 +50,11 @@ function mostrarTabla(){
         <tr>
             <td>${item.nombre}</td>
             <td>${item.cedula}</td>
-            <td>${item.fechaNacimiento}</td>
+            <td>${item.fecha}</td>
             <td>${item.edad}</td>
             <td>
                 <button type="button" onclick = "eliminarPersona(${item.cedula})">Cancelar</button>
-                <button type="button" onclick = "editarPersona(${item.cedula})">Editar</button>
+                <button type="button" onclick = "mostrarDatosEditar(${item.cedula})">Editar</button>
             </td>
       </tr>`
     })
@@ -61,10 +66,17 @@ function mostrarTabla(){
 //Eliminar una persona del array persona
 function eliminarPersona(cedula){
 
-    let result = personas.find(item => item.cedula == cedula);
-    elimanadas.push(result)
+    let validar = window.confirm('Deseas Eliminar');
+    if (validar === true) {
+        window.alert('Aceptar');
+    } else { 
+        window.alert('Rechazar');
+    }
 
-    localStorage.setItem('eliminada', JSON.stringify(elimanadas));
+    let result = personas.find(item => item.cedula == cedula);
+    eliminadas.push(result)
+
+    localStorage.setItem('eliminada', JSON.stringify(eliminadas));
 
     let personasfilter = personas.filter((item) => item.cedula != cedula)
     personas = personasfilter
@@ -77,17 +89,61 @@ function eliminarPersona(cedula){
 
 function mostrarPersonasEliminadas() {
     let result = "";
-    elimanadas.forEach((item) => {
+    eliminadas.forEach((item) => {
         result += `
         <div id="car2" class="card-body">
             <h5 class="card-title">${item.nombre}</h5>
-            <p class="card-text">${item.fechaNacimiento}</p>
+            <p class="card-text">${item.fecha}</p>
             <p class="card-text">${item.edad}</p>
         </div>`
     })
     let car = document.querySelector("#car");
     car.innerHTML = result;
 }
+
+
+function mostrarDatosEditar(cedula){
+
+    let guardar = document.getElementById('guardar')
+    guardar.style.display = "none"
+
+    editar.style.display = "block"
+
+    let validar = window.confirm('Deseas Editar');
+    if (validar === true) {
+        window.alert('Aceptar');
+    } else { 
+        window.alert('Rechazar');
+    }
+
+    let result = personas.find(item => item.cedula==cedula);
+
+    let nombre = document.getElementById('nombre')
+    let cedula2 = document.getElementById('cedula')
+    let fecha = document.getElementById('fecha')
+
+    nombre.setAttribute("value", result.nombre)
+    cedula2.setAttribute("value", result.cedula)
+    fecha.setAttribute("value", result.fecha)
+
+    editarPersona(result.cedula)
+}
+
+//Editar Persona
+function editarPersona(){
+        editar.addEventListener('submit', function(e){
+        e.preventDefault();
+
+        const materia2 = {
+            nombre: document.getElementById('nombre').value,
+            cedula: document.getElementById('cedula').value,
+            fecha: document.getElementById('fecha').value
+        }   
+        });  
+}
+
+
+
 
 
 
